01f964eeee9139e2a3c03404c1062f5f2d88e814
KrupaliS <krupali.shrivas@gmail.com>
2023-04-24 16:35:33 +0530

    demo file

---
+++ demo.java
@@ -0,0 +1,27 @@
+package practice26april;
+
+public class NewPalindrome {
+
+    public static void main(String[] args) {
+
+        String s = "MOM";
+
+        String rev = " ";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+            rev = rev + s.charAt(i);
+        }
+
+        // {
+        // System.out.println(rev);
+        // }
+
+        if (s.equals(rev))
+
+        {
+            System.out.println("Palindrom string");
+        }
+
+    }
+}
